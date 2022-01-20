70b0a4b9161016998fb9f88216ba5abfd588e904
Emily Burger <eburger939@gmail.com>
2022-01-20 10:59:20 -0600

    added requirements for product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-
-  },
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, 
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        product: id,
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        tag: id,
+      }
+    },
+},
   {
     sequelize,
     timestamps: false,
