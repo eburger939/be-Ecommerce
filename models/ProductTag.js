d7082f462514b1b86dfc292aca97fbc9a0b6ddca
Emily Burger <eburger939@gmail.com>
2022-01-20 10:57:27 -0600

    moved requirements from product tag model to tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,16 +6,7 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true, 
-      allowNull: false,
-    },
-    tag_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
+
   },
   {
     sequelize,
